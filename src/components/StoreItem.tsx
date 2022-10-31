715570412613b5155c16a31074b06985e34f09ab
Fred <fredsg22@hotmail.com>
2022-10-30 19:55:30 -0600

    Store page functionality added

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,5 @@
-import { Card } from "react-bootstrap"
-
+import { Button, Card } from "react-bootstrap"
+import { formatCurrency } from "../utilities/formatCurrency"
 
 type StoreItemsProps = {
     id: number
@@ -10,14 +10,33 @@ type StoreItemsProps = {
 
 export function StoreItem({ id, name, price, imgUrl }:
     StoreItemsProps) {
-    return <Card>
-        <Card.Img variant="top" src={imgUrl} height="200px"
-            style={{ objectFit: "cover" }} />
-        <Card.Body className="d-flex flex-colum">
-            <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
-                <span className="fs-2">{name}</span>
-                <span className="ms-2 text-muted">{price}</span>
-            </Card.Title>
-        </Card.Body>
-    </Card>
+    const quantity = 0
+    return (
+        <Card className="h-100">
+            <Card.Img variant="top" src={imgUrl} height="200px"
+                style={{ objectFit: "cover" }} />
+            <Card.Body className="d-flex flex-column">
+                <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                    <span className="fs-2">{name}</span>
+                    <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+                </Card.Title>
+                <div className="mt-auto">
+                    {quantity === 0 ? (
+                        <Button className="w-100">+ Add to Cart</Button>
+                    ) : <div className="d-flex flex-column align-items-center"
+                        style={{ gap: ".5rem" }}>
+                        <div className="d-flex align-items-center
+                            justify-content-center" style={{ gap: ".5rem" }}>
+                            <Button>-</Button>
+                            <div>
+                                <span className="fs-3">{quantity}</span> in cart
+                            </div>
+                            <Button>+</Button>
+                        </div>
+                        <Button variant="danger" size="sm">Remove</Button>
+                    </div>}
+                </div>
+            </Card.Body>
+        </Card>
+    )
 }
